83c01f568b093d28a8590a87216ff94bc9fb289c
maxence <maxence@MacBook-Air-de-Virginie.local>
2025-02-06 10:53:19 +0100

    creation Card.jsx et ajout a App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <Card></Card>
     </>
   )
 }
