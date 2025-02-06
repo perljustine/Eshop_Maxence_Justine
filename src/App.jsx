beb363a904e5e696b678180ade4a127d70fcce57
maxence <maxence@MacBook-Air-de-Virginie.local>
2025-02-06 14:30:20 +0100

    laisse moi prendre les upadates

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
 import './App.css'
-<<<<<<< HEAD
 import Header from './components/Header'
-=======
 import Card from './components/Card'
 import data from './assets/data.json'
->>>>>>> 4a6c230 (Céation de la carte et mise en place des props qui permettent d'avoir pour chaque carte des nom et des valeurs différentes.)
 
 function App() {
 
-  return (
-<<<<<<< HEAD
+    return (
     <>
-     <Header></Header>
-    </>
-  )
-}
-=======
-      <>
-              <div className="flex justify-center items-center w-full flex-wrap flex-row xl:grid xl:grid-cols-3 xl:justify-items-center">
-                  {
-                      data.map((item) => (
-                          <Card key={item.name} name={item.name} quantity={item.quantity} price={item.price}></Card>
-                      ))
-                  }
-              </div>
-          </>
-          )
-          }
->>>>>>> 4a6c230 (Céation de la carte et mise en place des props qui permettent d'avoir pour chaque carte des nom et des valeurs différentes.)
+         <Header></Header>
 
-          export default App
+          <div className="flex justify-center items-center w-full flex-wrap flex-row xl:grid xl:grid-cols-3 xl:justify-items-center">
+              {
+                  data.map((item) => (
+                      <Card key={item.name} name={item.name} quantity={item.quantity} price={item.price}></Card>
+                  ))
+              }
+          </div>
+    </>
+    )}
+    export default App
