e667708d65f547953618471f3af5474560359192
perljustine <justine.perl@hotmail.com>
2025-02-06 16:02:21 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
 import Card from './components/Card'
 import data from './assets/data.json'
 import Header from './components/Header'
@@ -22,17 +21,4 @@ function App() {
           }
 
           export default App
-    return (
-    <>
-         <Header></Header>
-
-          <div className="flex justify-center items-center w-full flex-wrap flex-row xl:grid xl:grid-cols-3 xl:justify-items-center">
-              {
-                  data.map((item) => (
-                      <Card key={item.name} name={item.name} quantity={item.quantity} price={item.price} img={item.img}></Card>
-                  ))
-              }
-          </div>
-    </>
-    )}
-    export default App
+ 
