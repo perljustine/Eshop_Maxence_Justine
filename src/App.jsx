1e6793d7ad7751a2df6794238887b6ed061673d4
perljustine <justine.perl@hotmail.com>
2025-02-06 16:02:21 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header'
 import Card from './components/Card'
 import data from './assets/data.json'
 
 function App() {
 
-    return (
-    <>
+  return (
+      <>
+              <Header />
+              <div className="flex justify-center items-center w-full flex-wrap flex-row xl:grid xl:grid-cols-3 xl:justify-items-center">
+                  {
+                      data.map((item) => (
+                          <Card key={item.name} name={item.name} quantity={item.quantity} price={item.price}></Card>
+                      ))
+                  }
+              </div>
+          </>
+          )
+          }
 
-          <div className="flex justify-center items-center w-full flex-wrap flex-row xl:grid xl:grid-cols-3 xl:justify-items-center">
-              {
-                  data.map((item) => (
-                      <Card key={item.name} name={item.name} quantity={item.quantity} price={item.price} img={item.img}></Card>
-                  ))
-              }
-          </div>
-    </>
-    )}
-    export default App
+          export default App
+ 
