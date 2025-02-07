8edcb5f324218142fe49c1b1170e802740a8842c
maxence <maxence@MacBook-Air-de-Virginie.local>
2025-02-07 09:17:21 +0100

    création du bouton panier avec la gestion des produits et en plus création des fonctionnalitées et modification de la boucle de creation des cards ( changement de nom pour que ca colle plus à ce que ca fait. J'ai aussi modifier les props pour y ajouter la fonction addToCart et item ( voir pour modifier les props plus tard) et supprimer le set items dans Card car ca empêchait d'être connecter l'un à l'autre donc je dis juste que stock=prod.quantity ce qui permet au deux d'être connecter l'un à l'autre et de gérer les changements.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,67 +1,42 @@
-import { useState } from 'react'
-
 // eslint-disable-next-line react/prop-types
-function Card({name, quantity, price, img}) {
-    const [stock, setStock] = useState(quantity)
+function Card({ prod, addToCart }) {
+    // ici on gère les stocks entre les card et l'app
+    const stock = prod.quantity;
 
-    const handleStock = ()=>{
-        if (stock > 0){
-            setStock(stock-1)
+    const handleStock = () => {
+        // la fonction se lance que si les stocks sont supp à 0
+        if (stock > 0) {
+            addToCart(prod);
         }
-    }
+    };
 
     return (
         <>
-            <div className={`relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md ${stock === 0 ? 'opacity-50' : ''}`}>
+            <div className={`relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md ${stock === 0 ? "opacity-50" : ""}`}>
                 <div className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl justify-center">
-                    <img className="object-cover" src={`/images/${img}`} alt="product image"/>
+                    <img className="object-cover" src={`/images/${prod.img}`} alt={prod.name} />
                     {stock === 0 && (
-                        <span className="absolute top-0 left-0 m-2 rounded-full bg-red-500 px-2 text-center text-sm font-medium text-white">Épuisé</span>
+                        <span className="absolute top-0 left-0 m-2 rounded-full bg-red-500 px-2 text-center text-sm font-medium text-white">
+                            Épuisé
+                        </span>
                     )}
                 </div>
                 <div className="mt-4 px-5 pb-5">
-                    <div>
-                        <h5 className="text-xl tracking-tight text-slate-900">{name}</h5>
-                    </div>
+                    <h5 className="text-xl tracking-tight text-slate-900">{prod.name}</h5>
                     <div className="mt-2 mb-5 flex items-center justify-between">
-                        <p>
-                            <span className="text-3xl font-bold text-slate-900">{price}€</span>
-                        </p>
-                        <div className="flex items-center">
-                            <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor"
-                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                            </svg>
-                            <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor"
-                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                            </svg>
-                            <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor"
-                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                            </svg>
-                            <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor"
-                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                            </svg>
-                            <svg aria-hidden="true" className="h-5 w-5 text-yellow-300" fill="currentColor"
-                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/>
-                            </svg>
-                            <span className={`mr-2 ml-3 rounded px-2.5 py-0.5 text-xs font-semibold ${stock === 0 ? 'bg-gray-500 text-white' : 'bg-yellow-200 text-black'}`}>{stock > 0 ? `Stock : ${stock}` : 'Rupture'}</span>
-                        </div>
+                        <p className="text-3xl font-bold text-slate-900">{prod.price}€</p>
+                            <span className={`mr-2 ml-3 rounded px-2.5 py-0.5 text-xs font-semibold ${stock === 0 ? "bg-gray-500 text-white" : "bg-yellow-200 text-black"}`}>
+                                {stock > 0 ? `Stock : ${stock}` : "Rupture"}
+                            </span>
                     </div>
-                    <button onClick={handleStock}  disabled={stock === 0} className={`flex w-full items-center justify-center rounded-md px-5 py-2.5 text-center text-sm font-medium text-white ${stock === 0 ? 'bg-red-500 cursor-not-allowed' : 'bg-slate-900 hover:bg-[#DF7BAA]'}`}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none"
-                             viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"/>
-                        </svg>
-                        Add to cart</button>
+                    {/*disable renvoit un booleén et permet d'empêcher l'utilisateur d'utiliser le bouton si la valeur est 0*/}
+                    <button onClick={handleStock} disabled={stock === 0} className={`flex w-full items-center justify-center rounded-md px-5 py-2.5 text-center text-sm font-medium text-white ${stock === 0 ? "bg-red-500 cursor-not-allowed" : "bg-slate-900 hover:bg-[#DF7BAA]"}`}>
+                        {stock === 0 ? "Épuisé" : "Ajouter au panier"}
+                    </button>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Card
+export default Card;
