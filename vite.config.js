fe72c1918ab2fac4cc5309cf30a7efb254adf8a1
maxence <maxence@MacBook-Air-de-Virginie.local>
2025-02-09 20:13:41 +0100

    test commande pour gérer reactjs

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Eshop_Maxence_Justine/",
   plugins: [react(),
   tailwindcss(),]
 
