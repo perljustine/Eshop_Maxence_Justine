c9d52151864d55f67dc71ac38f1002f264817c9c
maxence <maxence@MacBook-Air-de-Virginie.local>
2025-02-09 20:34:57 +0100

    même erreur avec le base pour le github page

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Eshop_Maxence_Justine/",
   plugins: [react(),
   tailwindcss(),]
 
